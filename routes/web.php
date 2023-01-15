1133e3a75bdb8b0ffbb5bc577b612a24641c3c74
Johan Hoyos <ci83rtda@live.com>
2023-01-15 13:57:08 -0500

    test 1

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
+Route::get('/guest/s/default', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
