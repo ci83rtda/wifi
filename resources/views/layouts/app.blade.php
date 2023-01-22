641c1416f60d3cccc34a359e6c102f349053def8
Johan Hoyos <ci83rtda@live.com>
2023-01-21 20:06:05 -0500

    test 2.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/js/app.js')
 </head>
 <body class="h-full">
-<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 px-8">
+<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 px-20">
     @yield('content')
 </div>
 </body>
