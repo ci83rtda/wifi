aad7a96073b75f850899e538138cab28720c8700
Johan Hoyos <ci83rtda@live.com>
2023-01-22 00:41:17 -0500

    test 2.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-50">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +7,7 @@
     <title>Hotspot</title>
     @vite('resources/js/app.js')
 </head>
-<body class="h-full bg-gray-50">
+<body class="h-full">
 <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 px-20">
     @yield('content')
 </div>
